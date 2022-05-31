56f590b9328186d7f47546a0e47922ae9d422041
12si27 <88251502+12si27@users.noreply.github.com>
2022-05-31 23:38:44 +0900

    Update database.sql

--- database.sql
+++ database.sql
@@ -24,6 +24,21 @@ CREATE TABLE `hourly_view` (
   `views` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+CREATE TABLE `locked` (
+  `id` varchar(10) NOT NULL,
+  `active` tinyint(1) NOT NULL,
+  `vid_key` varchar(512) DEFAULT NULL,
+  `allowed_user` varchar(512) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `login_log` (
+  `log_id` int(11) NOT NULL,
+  `id` varchar(10) NOT NULL,
+  `ip_address` varchar(20) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT current_timestamp(),
+  `success` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 CREATE TABLE `videos` (
   `file_loc` varchar(200) NOT NULL,
   `id` varchar(10) NOT NULL,
@@ -39,12 +54,21 @@ ALTER TABLE `hourly_view`
   ADD PRIMARY KEY (`id`,`date`,`hour`),
   ADD KEY `date` (`date`);
 
+ALTER TABLE `locked`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `login_log`
+  ADD PRIMARY KEY (`log_id`);
+
 ALTER TABLE `videos`
   ADD PRIMARY KEY (`id`),
   ADD KEY `id` (`id`),
   ADD KEY `last_checked` (`last_checked`),
   ADD KEY `views` (`views`);
 
+
+ALTER TABLE `login_log`
+  MODIFY `log_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=263;
 
 ALTER TABLE `hourly_view`
   ADD CONSTRAINT `hourly_view_ibfk_1` FOREIGN KEY (`id`) REFERENCES `videos` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
